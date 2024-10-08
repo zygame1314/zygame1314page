7ea7e6ae3e8e75157c298dc0ad87b7d0688cdc37
Yi <125733585+zygame1314@users.noreply.github.com>
2024-10-08 19:13:21 +0800

    Add files via upload

--- js/gamesFetch.js
+++ js/gamesFetch.js
@@ -34,12 +34,12 @@ function initGamesFetch() {
             if (game.background_image) {
                 gameImage.src = game.background_image;
             } else {
-                gameImage.src = 'https://cdn.jsdmirror.com/gh/zygame1314/zygame1314page@1.2.5/images/default-game-cover.png';
+                gameImage.src = 'https://cdn.jsdmirror.com/gh/zygame1314/zygame1314page@1.2.6/images/default-game-cover.png';
                 gameImage.classList.add('default-cover');
             }
             gameImage.alt = game.name || '未知游戏';
             gameImage.onerror = function () {
-                this.src = 'https://cdn.jsdmirror.com/gh/zygame1314/zygame1314page@1.2.5/images/default-game-cover.png';
+                this.src = 'https://cdn.jsdmirror.com/gh/zygame1314/zygame1314page@1.2.6/images/default-game-cover.png';
                 this.classList.add('default-cover');
             };
 
