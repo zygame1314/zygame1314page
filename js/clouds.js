b03643297cca66a4b29f28b7b3de2dfc11792598
Yi <125733585+zygame1314@users.noreply.github.com>
2024-10-08 19:10:26 +0800

    Add files via upload

--- js/clouds.js
+++ js/clouds.js
@@ -1,9 +1,9 @@
 function initClouds() {
     const cloudContainer = document.querySelector('.cloud-container');
     const cloudImages = [
-        'https://cdn.jsdmirror.com/gh/zygame1314/zygame1314page@1.2.4/images/cloud-image1.png',
-        'https://cdn.jsdmirror.com/gh/zygame1314/zygame1314page@1.2.4/images/cloud-image2.png',
-        'https://cdn.jsdmirror.com/gh/zygame1314/zygame1314page@1.2.4/images/cloud-image3.png'
+        'https://cdn.jsdmirror.com/gh/zygame1314/zygame1314page@1.2.5/images/cloud-image1.png',
+        'https://cdn.jsdmirror.com/gh/zygame1314/zygame1314page@1.2.5/images/cloud-image2.png',
+        'https://cdn.jsdmirror.com/gh/zygame1314/zygame1314page@1.2.5/images/cloud-image3.png'
     ];
     const cloudCount = 15;
 
