1a1d8a91c183fc711e234be661b24eda782ecb8b
Yi <125733585+zygame1314@users.noreply.github.com>
2024-10-13 14:11:09 +0800

    Add files via upload

--- js/live2d.js
+++ js/live2d.js
@@ -1,7 +1,7 @@
 window.onload = () => {
     new l2dViewer({
         el: document.getElementById('L2dCanvas'),
-        basePath: 'https://jsd.onmicrosoft.cn/gh/zygame1314/Live2dV3@1.0.4/assets',
+        basePath: 'https://jsd.onmicrosoft.cn/gh/zygame1314/Live2dV3@latest/assets',
         modelName: 'knight',
         width: 300,
         height: 300,
