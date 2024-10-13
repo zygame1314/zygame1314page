7134f5d52ec15814e5a6c86ce7a3f975ac4dbb8f
Yi <125733585+zygame1314@users.noreply.github.com>
2024-10-13 13:40:03 +0800

    Add files via upload

--- js/live2d.js
+++ js/live2d.js
@@ -4,6 +4,7 @@ window.onload = () => {
         basePath: 'https://jsd.onmicrosoft.cn/gh/zygame1314/Live2dV3@1.0.4/assets',
         modelName: 'knight',
         width: 300,
-        height: 300
+        height: 300,
+        mobileLimit: true
     })
 }
