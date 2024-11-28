0b4c4dec5636d4a0de9e2fe16060b4360fb4272d
Yi <125733585+zygame1314@users.noreply.github.com>
2024-11-28 23:28:23 +0800

    Add files via upload

--- api/games/recent-games.js
+++ api/games/recent-games.js
@@ -2,11 +2,6 @@ import https from 'https';
 import querystring from 'querystring';
 
 export default async function handler(req, res) {
-    // CORS 设置
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
--- api/location.js
+++ api/location.js
@@ -1,10 +1,6 @@
 import https from 'https';
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
