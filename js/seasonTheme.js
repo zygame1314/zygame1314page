9b54ab249f3419c97841ace4204ec91878217206
zygame1314 <zygame1314@gmail.com>
2025-03-01 03:08:56 +0800

    新增节日主题功能，支持多种节日问候语并更新相关样式

--- js/seasonTheme.js
+++ js/seasonTheme.js
@@ -9,7 +9,14 @@ const GREETINGS = {
     lanternFestival: "汤圆：麻薯的东方表亲 🏮<br>吃黑芝麻馅的自动组队",
     christmas: "对象还没找到？先和圣诞老人许个愿吧🎅<br>姜饼人正在赶来的路上",
     halloween: "社恐人cos幽灵最合适了👻<br>今晚不给糖就发老板表情包！",
-    valentines: "单身狗保护协会提醒：<br>本日宜关闭朋友圈 💘<br>恋爱脑请走VIP通道"
+    valentines: "单身狗保护协会提醒：<br>本日宜关闭朋友圈 💘<br>恋爱脑请走VIP通道",
+    qixi: "单身汪注意！今晚银河将上演大型狗粮投放现场🌌<br>星星代写情书服务限时开放（5星好评返现）💌",
+    nationalDay: "朋友圈摄影大赛2.0开启！🇨🇳<br>7天假期体验卡已到账，请注意避开人从众模式",
+    qingming: "青团：春天限定皮肤已更新🌸<br>踏青模式启动，小心柳絮偷袭！",
+    labor: "劳动最光荣，搬砖也能成英雄 🛠<br>系统提示：成就值+10086，摸鱼技能冷却中⏰",
+    teacher: "三尺讲台变直播间，粉笔进化成电子笔🖋<br>作业本批注比情书还长（老师辛苦了！🍎）",
+    children: "成年人请出示童年通行证🎈<br>今日限时返场：泡泡机模式/棒棒糖能量充满！",
+    newyear: "新年副本已加载 99%🎆<br>Flag回收站清空中，新年计划正在生成..."
 };
 
 function loadLunarScript() {
@@ -53,6 +60,14 @@ async function setSeasonTheme() {
             theme = 'mid-autumn';
             greeting = GREETINGS.midAutumn;
         }
+        else if (lunarMonth === 7 && lunarDay === 7) {
+            theme = 'qixi';
+            greeting = GREETINGS.qixi;
+        }
+        else if ((month === 4 && (day === 4 || day === 5))) {
+            theme = 'qingming';
+            greeting = GREETINGS.qingming;
+        }
     }
 
     if (!theme) {
@@ -67,6 +82,26 @@ async function setSeasonTheme() {
         else if (month === 2 && day === 14) {
             theme = 'valentines';
             greeting = GREETINGS.valentines;
+        }
+        else if (month === 10 && day >= 1 && day <= 7) {
+            theme = 'nationalDay';
+            greeting = GREETINGS.nationalDay;
+        }
+        else if (month === 5 && day >= 1 && day <= 5) {
+            theme = 'labor';
+            greeting = GREETINGS.labor;
+        }
+        else if (month === 9 && day === 10) {
+            theme = 'teacher';
+            greeting = GREETINGS.teacher;
+        }
+        else if (month === 6 && day === 1) {
+            theme = 'children';
+            greeting = GREETINGS.children;
+        }
+        else if (month === 1 && day === 1) {
+            theme = 'newyear';
+            greeting = GREETINGS.newyear;
         }
     }
 
