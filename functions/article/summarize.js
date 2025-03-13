addb9a5d583f96dba21dde855ace75e0787dcd54
zygame1314 <zygame1314@gmail.com>
2025-03-14 01:26:01 +0800

    优化文章摘要功能，增加最大令牌数至2048，以提升内容生成能力

--- functions/article/summarize.js
+++ functions/article/summarize.js
@@ -42,7 +42,7 @@ export async function onRequestPost(context) {
                             ${truncatedContent}`
                     }
                 ],
-                max_tokens: 400,
+                max_tokens: 2048,
                 temperature: 0.7,
                 top_p: 0.95
             })
