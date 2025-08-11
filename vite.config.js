143d4cd9cb582cd3b9289d782bc1df80b936cc17
zygame1314 <zygame1314@gmail.com>
2025-08-11 13:28:16 +0800

    Add login page entry to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         admin: resolve(__dirname, 'admin/index.html'),
+        login: resolve(__dirname, 'admin/login.html'),
       },
       output: {
         manualChunks(id) {
