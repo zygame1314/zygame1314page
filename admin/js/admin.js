2caa18fcefea1cf3fcd24b1ae949a7fddab93ec8
zygame1314 <zygame1314@gmail.com>
2025-09-14 13:25:50 +0800

    fix: 移除 loadDashboard 中不必要的统计数据更新

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -205,8 +205,6 @@ class AdminSystem {
     }
     async loadDashboard() {
         try {
-            const stats = await api.getStatistics();
-            this.updateStatistics(stats);
             const donations = await api.donations.getList(1, 5);
             this.updateRecentDonations(donations.data || []);
         } catch (error) {
